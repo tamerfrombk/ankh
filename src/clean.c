06e0fdfbcbf1529204e6ed69bc3f6a91985f6dd4
Tamer Aly <taly.dev75@gmail.com>
2021-03-21 14:17:30 -0400

    add a way to split a line by whitespace

--- src/clean.c
+++ src/clean.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 #include <readline/readline.h>
 
@@ -8,6 +10,41 @@
 
 // TODO: handle signal to gracefully kill shell
 volatile int done = 0;
+
+// TODO: move this somewhere we can test it
+char **split_by_whitespace(const char *str)
+{
+    // TODO: expand number of arguments or customize them
+    char **tokens = calloc(10, sizeof(*tokens));
+    if (tokens == NULL) {
+        // TODO: OOM error
+    }
+
+    int t = 0;
+    for (char c = *str; c != '\0'; c = *str) {
+        if (isspace(c)) {
+            ++str;
+            continue;
+        }
+
+        // find the end of the current word
+        int len = 0;
+        for (const char *eow = str; *eow != '\0' && !isspace(*eow); ++eow) {
+            ++len;
+        }
+
+        char *token = strndup(str, len);
+        if (token == NULL) {
+            // TODO: OOM error
+        }
+
+        str += len;
+
+        tokens[t++] = token;
+    }
+
+    return tokens;
+}
 
 int shell_loop(int argc, char **argv)
 {
@@ -25,6 +62,12 @@ int shell_loop(int argc, char **argv)
             free(line);
         } else {
             printf("you entered '%s'\n", line);
+            char **tokens = split_by_whitespace(line);
+            for (char **t = tokens; *t != NULL; ++t) {
+                puts(*t);
+                free(*t);
+            }
+            free(tokens);
             free(line);
         }
     }
