40f119b618ca814fd1c95043561b5da29b22e9da
Tamer Aly <taly.dev75@gmail.com>
2021-04-28 17:13:18 -0400

    remove string.{h,cc}

--- include/fak/string.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-
-std::vector<std::string> split_by_whitespace(const std::string& str);
-
-std::string join(const std::vector<std::string>& strs, const std::string& joiner);
